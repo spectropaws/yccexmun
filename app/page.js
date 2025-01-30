b179821e2104f7cc8713c646ec40030189b3922e
Rishabh Gokhe <rishabhgokhe2004@gmail.com>
2025-01-30 19:50:10 +0530

    created footer,about section and date section

--- app/page.js
+++ app/page.js
@@ -1,26 +1,30 @@
 'use client'
 import HeroSec from "../components/hero-sec";
 import InitialLoader from "../components/initial-loader";
-import Carousel from './components/Carousel';
-import Navbar from './components/Navbar'; 
-import './components/ui/styles.css';
-import './styles/globals.css';
-
+import Carousel from "./components/Carousel";
+import Navbar from "./components/Navbar";
+import "./components/ui/styles.css";
+import "./styles/globals.css";
+import ContentSection from "../components/ContentSection";
+import ImportantDates from "../components/ImportantDates";
+import Footer from "../components/Footer";
 
 const Page = () => {
   return (
     <>
-          <Navbar />
-
+      <Navbar />
       <InitialLoader />
       <div className="page-container">
         <div className="px-1 py-2 sm:px-2 sm:py-3  md:py-6 md:px-10">
-        <HeroSec />
-        <Carousel />
+          <HeroSec />
+          <Carousel />
+          <ContentSection />
+          <ImportantDates />
+        </div>
       </div>
-      </div>
+      <Footer />
     </>
   );
-}
+};
 
 export default Page;
