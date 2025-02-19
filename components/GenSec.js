f473a1f805eb92b1844c161fd85397cdc5677f9b
nastya <nastyax0aaaaa@gmail.com>
2025-02-19 18:05:57 +0530

    justified

--- components/GenSec.js
+++ components/GenSec.js
@@ -2,9 +2,9 @@ import Image from "next/image";
 
 export default function LetterPage() {
   return (
-    <div className="px-2 sm:px-2 md:px-10 py-8"> 
+    <div className="px-2 sm:px-2 md:px-6 py-6"> 
       <div className="gen-sec-letter-container shadow-lg rounded-2xl overflow-hidden max-w-none sm:mx-auto">
-        <section className="gen-sec-letter flex flex-col lg:flex-row items-center lg:items-center gap-6 p-4 sm:p-2 md:p-10">
+        <section className="gen-sec-letter flex flex-col lg:flex-row items-center lg:items-center gap-4 p-4 sm:p-2 md:p-8">
           
           <div className="text-left w-full lg:w-1/2">
             <h2 className="text-3xl md:text-4xl font-bold leading-tight">
@@ -12,13 +12,13 @@ export default function LetterPage() {
               Message from the Secretariat General
             </h2>
             <p className="mt-6 font-semibold">Dear Delegates and Honored Guests,</p>
-            <p className="mt-4 leading-relaxed">
+            <p className="mt-4 leading-relaxed text-justify hyphens-auto">
               I take immense pride in welcoming you to the maiden edition of YCCE X MUN, an event launched in honor of the Ruby Jubilee Celebrations of Yeshwantrao Chavan College of Engineering. At this point where YCCE marks its 40 years of excellence, we set out on a journey beyond academics—diplomacy, leadership, and global awareness.
             </p>
-            <p className="mt-4 leading-relaxed">
+            <p className="mt-4 leading-relaxed text-justify hyphens-auto">
               Model United Nations is just a simulation that has much more to it: it is a stage where young leaders hone their critical thinking skills, learn negotiation techniques, and craft solutions in committee rooms that are still viable beyond those rooms. Words have power, challenges are presented to perceptions, and consensus is built here.
             </p>
-            <p className="mt-4 leading-relaxed">
+            <p className="mt-4 leading-relaxed text-justify hyphens-auto">
               As you step into this conference, I urge you to do so with conviction, listen with an open mind, and debate with the intent of progress. Let this MUN be a reminder that the world is shaped by those who dare to question, who choose to engage, and who believe in the power of ideas.
             </p>
             <p className="mt-6 font-semibold">Wishing you all an inspiring and transformative experience.</p>
